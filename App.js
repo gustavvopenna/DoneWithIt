d2e95c6507327361841937e5f55acbb5dcc5421b
gustavvopenna <gustavo.pema@gmail.com>
2020-05-30 16:31:43 -0500

    Practice flexbox

--- App.js
+++ App.js
@@ -1,19 +1,58 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { View, Platform, StyleSheet, Image, Text } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        source={require('./assets/background.jpg')}
+        style={styles.splash}
+      />
+      <View style={styles.logoContainer}>
+        <View style={styles.logoWrapper}>
+          <Image
+            source={require('./assets/logo-red.png')}
+            style={styles.logo}
+          />
+          <Text style={{ marginTop: 15 }}>Sell what you don't need</Text>
+        </View>
+      </View>
+      <View style={styles.login} />
+      <View style={styles.register} />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+  splash: {
+    width: '100%',
+    flex: 8,
+  },
+  logoContainer: {
+    position: 'absolute',
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    top: 80,
+  },
+  logoWrapper: {
+    alignItems: 'center',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  login: {
+    backgroundColor: 'lightcoral',
+    width: '100%',
+    flex: 1,
+  },
+  register: {
+    backgroundColor: 'skyblue',
+    width: '100%',
+    flex: 1,
+  },
+})
