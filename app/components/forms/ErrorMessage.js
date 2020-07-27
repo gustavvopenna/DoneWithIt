e1145751b431db8cd2b2ce3dff458d1f42a39a5b
gustavvopenna <gustavo.pema@gmail.com>
2020-07-26 22:25:08 -0500

    Add index file to forms folder to export components inside

--- app/components/forms/ErrorMessage.js
+++ app/components/forms/ErrorMessage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { StyleSheet } from 'react-native'
 
-import AppText from './AppText'
+import AppText from '../AppText'
 
 const ErrorMessage = ({ error, visible }) => {
   if (!error || !visible) return null
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import { Image, StyleSheet } from 'react-native'
-import { Formik } from 'formik'
 import * as Yup from 'yup'
 
 import Screen from '../components/Screen'
-import AppFormField from '../components/AppFormField'
-import SubmitButton from '../components/SubmitButton'
-import AppForm from '../components/AppForm'
+import { AppForm, AppFormField, SubmitButton } from '../components/forms'
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label('Email'),
