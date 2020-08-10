7b46cbb3943ad05fc28090b00efe8b3af7685d3d
gustavvopenna <gustavo.pema@gmail.com>
2020-08-09 21:11:45 -0500

    Add support for numberOfLines prop

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -5,32 +5,8 @@ import { Text } from 'react-native'
 
 import defaultStyles from '../config/styles'
 
-function AppText({ children, type = 'text', style }) {
-  return <Text style={[defaultStyles.text, style]}>{children}</Text>
+function AppText({ children, type = 'text', style, ...otherProps }) {
+  return <Text style={[defaultStyles.text, style]} {...otherProps}>{children}</Text>
 }
-
-// const styles = StyleSheet.create({
-//   text: {
-//     fontSize: 18,
-//     fontWeight: 'bold',
-//     fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Avenir',
-//   },
-//   base: {
-//     fontSize: 16,
-//     fontWeight: '400',
-//     fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Avenir',
-//   },
-//   sm: {
-//     fontSize: 14,
-//     fontWeight: '400',
-//     color: 'dodgerblue',
-//     fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Avenir',
-//   },
-//   cardSubtitle: {
-//     fontSize: 14,
-//     color: '#20b2aa',
-//     fontWeight: '500',
-//   }
-// })
 
 export default AppText
